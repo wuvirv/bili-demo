6629e2f8627168a2a5966b55ec79a11b844a08ad
wuvirv <wuvirv@qq.com>
2020-09-14 14:37:28 +0800

    完成登录功能

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Register from '../views/Register'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'register',
     component: Register
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
 ]
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,42 +1,35 @@
 <template>
   <div>
-    <bl-text text="注册bilibili" />
-    <van-form @submit="onSubmit" ref="registerRef">
+    <bl-text text="登录bilibili" />
+    <van-form @submit="onSubmit">
       <van-field
-        v-model="registerForm.name"
-        name="name"
-        label="昵称"
-        placeholder="请输入昵称"
-        :rules="[{ required: true, message: '' }, { pattern: nameReg, message: '' }]"
+        v-model="loginForm.username"
+        name="账号"
+        label="账号"
+        placeholder="请输入你的账号"
+        :rules="[{ required: true, message: '' }, { pattern: usernameReg, message:'' }]"
         class="margin-top"
         label-width="16.667vw"
       />
       <van-field
-        v-model="registerForm.username"
-        name="账号"
-        label="账号"
-        placeholder="请输入账号"
-        :rules="[{ required: true, message: '' }, { pattern: usernameReg, message:'' }]"
-        label-width="16.667vw"
-      />
-      <van-field
-        v-model="registerForm.password"
+        v-model="loginForm.password"
         type="password"
         name="密码"
         label="密码"
-        placeholder="6～16个字符，区分大小写"
+        placeholder="请输入密码"
         :rules="[{ required: true, message: '' }, { pattern: passwordReg, message: '' }]"
         label-width="16.667vw"
       />
+      <p class="forget">忘记密码</p>
       <div style="margin: 4.444vw;">
         <div class="btn">
-          <van-button round plain @click="$router.push('/login')">登录</van-button>
-          <van-button round type="info" native-type="submit">同意并注册</van-button>
+          <van-button round plain @click="$router.push('/register')">注册</van-button>
+          <van-button round type="info" native-type="submit">登录</van-button>
         </div>
       </div>
     </van-form>
     <p>
-      完成注册即代表你同意<span>用户协议</span>和<span>隐私政策</span>
+      登录即代表你同意<span>用户协议</span>和<span>隐私政策</span>
     </p>
   </div>
 </template>
@@ -45,24 +38,21 @@
 export default {
   data () {
     return {
-      registerForm: {
-        name: '',
+      loginForm: {
         username: '',
         password: ''
       },
-      nameReg: /[\u4e00-\u9fa5_a-zA-Z0-9_]{4,15}/,
       usernameReg: /^[0-9a-zA-Z]{4,15}$/,
       passwordReg: /^[0-9a-zA-Z]{6,15}$/
     }
   },
   methods: {
     async onSubmit () {
-      const { data: res } = await this.$http.post('register', this.registerForm)
+      const { data: res } = await this.$http.post('login', this.loginForm)
       console.log(res)
-      if (res.code === 302) return this.$notify({ type: 'danger', message: res.msg })
-      if (res.code !== 200) return this.$notify({ type: 'danger', message: '注册失败' })
-      this.$notify({ type: 'success', message: '注册成功' })
-      this.$router.push('/login')
+      if (res.code !== 200) return this.$notify({ type: 'danger', message: '用户名或密码错误' })
+      this.$notify({ type: 'success', message: '登录成功' })
+      this.$router.push('/')
     }
   }
 }
@@ -70,13 +60,12 @@ export default {
 
 <style lang="less" scoped>
 .margin-top {
-  margin: 4.167vw 0;
+  margin-top: 4.167vw;
 }
 .btn {
   display: flex;
   justify-content: center;
   .van-button {
-    // width: 45.278vw;
     flex: 1;
     margin: 0 1.389vw;
     height: 11.667vw;
@@ -107,5 +96,10 @@ p {
   span {
     color: #fb7299;
   }
+}
+.forget {
+  color: #fb7299;
+  text-align: right;
+  margin: 2.778vw 2.778vw 6.944vw 0;
 }
 </style>
